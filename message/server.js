d392998f96baeed72d0c2ec13207ae8e73a3c360
jackobhailouny <jackob.ha2016@gmail.com>
2023-05-24 08:06:53 -0400

    add express and change from http to express

--- message/server.js
+++ message/server.js
@@ -1,6 +1,18 @@
 const http = require('http');
-const {handler} = require('./routes');
+const express = require('express')
 
-const server = http.createServer( handler);
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+
+})
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
